e345c9903a5af8b748ec39d832bd8f3c8d8e56b4
Rajkumar Gajbhiye <109793318+RajkumarGajbhiye@users.noreply.github.com>
2025-02-17 18:25:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,4 +68,69 @@ function findSquare(n) {
     return square;
   }
   console.log(findSquare(2));
-  
+
+/** Que.1.Write a JavaScript function that reverse a number.**
+
+**Example
+Sample Input:
+32243
+Expected Output: 
+34223**
+
+Explanation:
+Assume n = 1000.
+Convert a number to a string :
+Code : -> n = n + "";
+Note : There are different ways to convert number to string :
+
+String literal -> str = "" + num + "";
+String constructor -> str = String(num);
+toString -> str = num.toString();
+String Literal simple -> str = "" + num;
+The split() method is used to split a String object into an array of strings by separating the string into substrings.
+Code : console.log('1000'.split(""));
+Output : ["1", "0", "0", "0"]
+
+The reverse() method is used to reverse an array in place. The first array element becomes the last and the last becomes the first.
+Code : console.log(["1", "0", "0", "0"].reverse());
+Output : ["0", "0", "0", "1"]
+
+The join() method is used to join all elements of an array into a string.
+Code : console.log(["1", "0", "0", "0"].reverse().join(""));
+The Number constructor contains constants and methods for working with numbers. Values of other types can be converted to numbers using the Number() function.
+Output : 1*/
+
+
+function ReverseANumber(n) {
+    n = n + "";
+    return n.split("").reverse().join("");
+  }
+  console.log(Number(ReverseANumber(32243)));
+
+
+
+ /** Que.2. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.**
+
+**Sample Output:**
+
+**"0 is even"
+"1 is odd"
+"2 is even"
+__________
+and so on.**/
+
+function IterateFrom0To15CheckoddAndEvenNumber() {
+    for (let n = 0; n <= 15; n++)
+      if (n === 0) {
+        console.log(n + " is even ");
+      }
+      else if (n % 2 == 0) {
+        console.log(n + " is even ");
+      }
+      else {
+        console.log(n + " is odd ");
+      }
+  }
+  IterateFrom0To15CheckoddAndEvenNumber();
+
+
